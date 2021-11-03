ede102b4feb8f9f50f2144692337620b4f59d5bc
Nathan <nathaniel.mendez@upr.edu>
2021-11-03 18:33:42 -0400

    Added error handling to the code as well as some basic comments.

--- HissuiLexer.py
+++ HissuiLexer.py
@@ -1,33 +1,42 @@
 from sly import Lexer
 
 
+# Creating a Lexer who inherits from sly's lexer.oy class
 class HissuiLexer(Lexer):
+    # Inputting the tokens that the parser will be able to use.
     tokens = {ID, NUMBER, EQUAL, ASSIGN, FOR, IF, ELSE, ELSEIF, IN, SQUARE
         , CIRCLE, RECTANGLE, TRIANGLE, VECTOR, MATRIX, NEW, RP, LP, RB, LB, COMMA, COLON, RETURN}
 
+    # Lexer can read letters and combinations of letters and numbers
     ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
+    # ignore whitespaces or tabs.
     ignore = ' \t'
 
+    # Reads single digits or multiple digits.
     NUMBER = r'\d+'
 
+    # making the values inside the token integers
     def NUMBER(self, t):
         t.value = int(t.value)
         return t
 
+    # stating the operators
     literals = {'+', '-', '/', '*', '^'}
 
+    # establishing function characters
     EQUAL = r'=='
     ASSIGN = r'='
-
     COMMA = r','
     COLON = r':'
 
+    # adding parenthesis and bracket recognition
     LP = r'\('
     RP = r'\)'
     LB = r'\['
     RB = r'\]'
 
+    # implementing standard language clauses.
     ID['if'] = IF
     ID["else"] = ELSE
     ID['elif'] = ELSEIF
@@ -35,24 +44,29 @@ class HissuiLexer(Lexer):
     ID['in'] = IN
     ID['return'] = RETURN
 
+    # Adding different functional objects
     ID['square'] = SQUARE
     ID["rectangle"] = RECTANGLE
     ID['triangle'] = TRIANGLE
     ID['circle'] = CIRCLE
-
     ID['matrix'] = MATRIX
     ID['vector'] = VECTOR
 
+    # new token for object creation
     ID['new'] = NEW
 
-    def error(self,t):
-        print("Illegal character '%s'"%t.value[0])
+    # error handling
+    def error(self, t):
+        print("Illegal character '%s'" % t.value[0])
         self.index += 1
 
+
 if __name__ == '__main__':
-    data = "circle c = new circle(21)" \
-           "circle j = new circle(4)" \
-           "if(c == j) return true @"
     lexer = HissuiLexer()
-    for tok in lexer.tokenize(data):
-        print(tok)
+    while True:
+        inp = input("Hissui> ")
+        if inp:
+            for tok in lexer.tokenize(inp):
+                print("Type: %r Value: %r" % (tok.type, tok.value))
+        else:
+            break
